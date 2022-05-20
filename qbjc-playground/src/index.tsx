26da12aa131e5758b9e7b34a976a76069eeae8d4
Chuan Ji <chuan@jichu4n.com>
2022-05-20 15:06:26 -0700

    Add Segment analytics.

--- qbjc-playground/src/index.tsx
+++ qbjc-playground/src/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './app';
+import initializeSegment from './segment';
 
 ReactDOM.render(
   <React.StrictMode>
@@ -8,3 +9,4 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
+initializeSegment();
